0cb97bf2dc93946693ef5d7a08d55092c661f427
Evgeny Chukreev <Evgeny.Chukreev@gmail.com>
2021-04-30 01:25:07 +0200

    ...

--- src/js/server/app/klipper/KlipperBasicInfoService.ts
+++ src/js/server/app/klipper/KlipperBasicInfoService.ts
@@ -1,16 +1,27 @@
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable @typescript-eslint/naming-convention */
 import { Inject, Injectable } from 'injection-js';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { ENV_APP_VERSION } from 'server/akjs/core/env_tokens';
 import { LifecycleEvents } from 'server/akjs/core/LifecycleEvents';
 import { Logger } from 'server/akjs/core/Logger';
+import { asPrinterAttributeObservable } from './asPrinterAttributeObservable';
 import { KlipperCommService } from './KlipperCommService';
 import { KlipperProtocolService } from './KlipperProtocolService';
 import { KlipperUtils } from './KlipperUtils';
 
+const INFO_CMD_TIMEOUT_MS = 1000;
+
 @Injectable()
 export class KlipperBasicInfoService {
+    private readonly _stateSubj = new BehaviorSubject<string | null>(null);
+    public readonly state$: Observable<string | null> = asPrinterAttributeObservable({
+        attrSubj: this._stateSubj,
+        disconnected$: this._klipperCommService.disconnected$,
+    });
+
     public constructor(
+        private readonly _klipperCommService: KlipperCommService,
         private readonly _klipperProtocolService: KlipperProtocolService,
         private readonly _logger: Logger,
         private readonly _klipperUtils: KlipperUtils,
@@ -26,12 +37,11 @@ export class KlipperBasicInfoService {
     }
 
     private _handleConnect(): void {
-        // TODO: timeout value
         this._klipperProtocolService
             .makeKlipperRequest({
                 method: 'info',
                 params: { client_info: { name: 'akki', version: this._appVersion } },
-                timeoutMs: 1000,
+                timeoutMs: INFO_CMD_TIMEOUT_MS,
             })
             .subscribe((outcome) => {
                 if (outcome.kind === 'result') {
@@ -48,7 +58,7 @@ export class KlipperBasicInfoService {
                 params: { objects: { webhooks: ['state', 'state_message'] } },
             })
             .subscribe((outcome) => {
-                console.log('state', outcome);
+                console.log(outcome);
             });
 
         this._klipperProtocolService
@@ -60,19 +70,16 @@ export class KlipperBasicInfoService {
         this._klipperProtocolService
             .subscribeKlipper({
                 method: 'objects/subscribe',
-                // TODO: Make it more flexible via variable...
-                params: { objects: { extruder: ['temperature'], heater_bed: ['temperature'] } },
+                params: { objects: { extruder: ['temperature', 'target'], heater_bed: ['temperature', 'target'] } },
             })
             .subscribe((outcome) => {
                 console.log('temp', outcome);
             });
 
-        /*
-        TODO:
         this._klipperProtocolService
             .subscribeKlipper({ method: 'gcode/subscribe_output', params: {} })
             .subscribe((outcome) => {
                 console.log('gcode output', outcome);
-            });*/
+            });
     }
 }
